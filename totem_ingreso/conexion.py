e19f099dcc5721fbf5531c8e479d8d9ae5f514cb
Seba <seba.avergara@gmail.com>
2023-07-04 00:06:50 -0400

    cambios menores

--- totem_ingreso/conexion.py
+++ totem_ingreso/conexion.py
@@ -8,9 +8,7 @@ puerto = 'COM3'  # Cambia esto según el puerto que estés utilizando
 velocidad = 9600  # Ajusta la velocidad según las especificaciones del lector RFID
 dbconn = db.DBConnection()
 dbconn.connect()
-arduino = pyfirmata.Arduino('COM3')
-ledVerde = arduino.get_pin('a:5:o')
-ledRojo = arduino.get_pin('a:3:o')
+
 
 try:
     # Abre el puerto serial
@@ -27,21 +25,16 @@ try:
             uid = line.split(':')[1]
             print(f"UID recibido: {uid}")
             # Consultar en la base de datos si el UID recibido existe
-            query = 'SELECT * FROM "User" WHERE rfid = %s'
+            query = 'SELECT * FROM user WHERE rfid = %s'
             params = (uid,)
             results = dbconn.execute_query(query, params)
             if len(results) > 0:
                 user_rfid = results[0][6]
-                if(user_rfid == uid):
-                    print("Acceso concedido")
-                    #arduino.digital[ledVerde].write(1)
-                    ledVerde.write(1)
-                    time.sleep(3)  
+                user_membresia = results[0][5]
+                if(user_rfid == uid and user_membresia == True):
+                    print("Acceso concedido")  
             else:
                 print("Acceso denegado")
-                #arduino.digital[ledRojo].write(1)
-                ledRojo.write(1)
-                time.sleep(3)
         
 except KeyboardInterrupt:
     # Cierra el puerto serial y finaliza el programa si se presiona Ctrl+C
